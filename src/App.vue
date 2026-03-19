cd204f639a8ea28e84299cedacf83d079cf9e557
Alina Bakhvalova <a.bahvalova@inetstudio.ru>
2026-03-19 16:36:39 -0400

    Шаблоны

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
 const a = 10;
 let b = 20;
+const d = { a: 30 };
+let e = "<a href='/'>link</a>";
 </script>
 
 <template>
   <header>{{ a }}</header>
   <main>{{ b }}</main>
+  <div v-html="e"></div>
+  <footer>{{ d.a }}</footer>
 </template>
 
 <style scoped></style>
